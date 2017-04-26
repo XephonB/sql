7872eb26bae8480d9d617b55ed81a58c4ba0e108
Benedicto <isc.benedicto@gmail.com>
2017-04-26 11:21:05 -0500

    basico del sql

--- SPVentasHaffir2.sql
+++ SPVentasHaffir2.sql
@@ -1,17 +1,17 @@
-create PROCEDURE SP_VentasHaffir2 @Finicio DATE, @Ffinal DATE
+CREATE PROCEDURE SP_VentasHaffir2 @Finicio DATE, @Ffinal DATE
 
 AS BEGIN
---borrar
+----borrar
 --DECLARE @Finicio DATE, @Ffinal DATE
 --SET @Finicio='2016-01-12'
 --SET @Ffinal='2017-08-12'
---borrar
+----borrar
 
-SELECT Importe,Saldo,FechaEmision,Vencimiento,MovID,Mov,EnviarA,Nombre,
-'Estatus'=CASE WHEN EstatusCxc='' THEN LEFT(Estatus,3) ELSE LEFT(EstatusCxc,3) END,
+SELECT Importe,Saldo,FechaEmision,MovID,Mov,EnviarA,Nombre,
+'Estatus'=CASE WHEN EstatusCxc='' THEN Estatus ELSE EstatusCxc END,
 'MovPago'=CASE WHEN Condicion='Contado' THEN Condicion ELSE MovPago END,MovIDPago
  FROM(
-	SELECT (Venta.Importe+Venta.Impuestos)Importe,CAST(Venta.FechaEmision AS DATE)FechaEmision,CAST(Venta.Vencimiento AS date)Vencimiento,Venta.MovID,Venta.Mov,ISNULL(Venta.EnviarA,'')EnviarA,ISNULL(CteEnviarA.Nombre,'')Nombre,Venta.Estatus,Venta.Condicion
+	SELECT (Venta.Importe+Venta.Impuestos)Importe,CAST(Venta.FechaEmision AS DATE)FechaEmision,Venta.MovID,Venta.Mov,ISNULL(Venta.EnviarA,'')EnviarA,ISNULL(CteEnviarA.Nombre,'')Nombre,Venta.Estatus,Venta.Condicion
 	,ISNULL((SELECT C.Saldo FROM Cxc C WHERE C.MovID=Venta.MovID),'')Saldo
 	--,MF1.DID,MF1.DMovID--BORRAR
 	,ISNULL((SELECT Estatus FROM Cxc WHERE ID=MF1.DID),'')EstatusCxc
@@ -21,9 +21,11 @@ SELECT Importe,Saldo,FechaEmision,Vencimiento,MovID,Mov,EnviarA,Nombre,
 		LEFT JOIN MovFlujo MF2 ON MF1.DID=MF2.OID AND MF1.DMov=MF2.OMov AND MF1.DMovID=MF2.OMovID AND MF2.Cancelado=0
 		WHERE Venta.Cliente='PG001' AND Venta.Mov IN ('Factura','FacturaPromPzo Expo','Factura Mayoreo Inst','Factura Expo','Factura Prom. Plazo')
 		AND CAST(Venta.FechaEmision AS date) BETWEEN @Finicio AND @Ffinal 
-)TFac
+)TFa
 ORDER BY FechaEmision,MovID --MAY26422 --1556574,MAY32316
 
 END
 
---exec SP_VentasHaffir2 @Finicio='2016-08-12',@Ffinal='2016-08-12'
+/*
+exec SP_VentasHaffir2 @Finicio='2016-08-12',@Ffinal='2016-08-12'
+*/
