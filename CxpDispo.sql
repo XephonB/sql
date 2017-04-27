de15bb8b9b003a2bbf510655eb8cbecff6c94a58
Benedicto <isc.benedicto@gmail.com>
2017-04-27 12:41:09 -0500

    Codigo base

---
+++ CxpDispo.sql
@@ -0,0 +1,21 @@
+--CREATE PROCEDURE CxpDispo @FechaD date, @FechaA date
+--AS BEGIN
+
+--Try me
+Declare @FechaD date, @FechaA date;
+SET @FechaD='01-01-2017'
+SET @FechaA='01-03-2017'
+--Try me
+
+	SELECT Cxp.Proveedor,Prov.Nombre,SUM(Cxp.Importe)Importe,LEFT(CAST(Cxp.FechaEmision AS date),7)FechaEmision FROM Cxp 
+	INNER JOIN Prov ON Cxp.Proveedor=Prov.Proveedor
+	WHERE Cxp.Mov='Disposicion Consig.' 
+	AND LEFT(CAST(Cxp.FechaEmision AS date),7) BETWEEN LEFT(@FechaD,7) AND LEFT(@FechaA,7)
+	--AND (DATEPART(MONTH,Cxp.FechaEmision) = DATEPART(MONTH,@Fecha) AND DATEPART(YEAR,Cxp.FechaEmision) = DATEPART(YEAR,@Fecha))
+	GROUP BY Cxp.Proveedor,Prov.Nombre,LEFT(CAST(Cxp.FechaEmision AS date),7)
+	ORDER BY FechaEmision,Cxp.Proveedor;
+--END
+
+/*
+EXEC CxpDispo @FechaD='01-01-2017, @FechaA='01-03-2017'
+*/
